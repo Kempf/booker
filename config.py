db2e0f4232e1623860b1189bd83a960b0865a7ec
Paul Apelt <lt.kempf@gmail.com>
2014-08-21 00:25:45 +1000

    config module bugfix

--- config.py
+++ config.py
@@ -2,28 +2,29 @@
 # Paul Apelt u55628225
 import datetime
 import os.path
-log = initlog()
 def checkline(line):
+    """Checks if the config line is not a comment"""
     if line[0] != '#' and line.strip():
         return True
-    else
+    else:
         return False
 def initlog():
-''' Initiates log '''
+    """Initiates log"""
     log = open('log','a')
-    log.write("{}: {} log".format(datetime.date.today().isoformat(),__name__))
+    log.write("{}: {} log.\n"\
+            .format(datetime.date.today().isoformat(),__name__))
     return log
 def fileopen(filename,mode):
-''' Utility file open with exception catching '''
+    """Utility file open with exception catching"""
     try:
         f = open(filename,mode)
-        print("Opened {} as {}".format(filename,mode),file = log)
+        print("Opened {} as {}\n".format(filename,mode),file = log)
     except FileNotFoundError:
-        print("File '{}' not found!".format(filename),file = log)
+        print("File '{}' not found!\n".format(filename),file = log)
         exit()
     return f
-def timetable(filename,log):
-''' Parse timetable config and return it as a list '''
+def timetable(filename):
+    """Parse timetable config and return it as a list"""
     timelist = []
     f = fileopen(filename,'r')
     for line in f:
@@ -36,28 +37,29 @@ def timetable(filename,log):
                     try:
                         hourlist[i] = int(hour)
                     except ValueError:
-                        print("Bad config format at line '{}'!"\
+                        print("Bad config format at line '{}'!\n"\
                                 .format(line.strip()),file = log)
                         exit()
                 breaklist.append(hourlist)
             timelist.append(breaklist)                    
     return timelist
 def bookings(filename):
-''' Parses the bookings list and returns it as a list '''
+    """Parses the bookings list and returns it as a list"""
     booklist = []
     if os.path.isfile(filename):
         f = fileopen(filename,'r')
         for line in f:
             if checkline(line):
                 linelist = line.split(',')
-                hourlist = linelist[-1]
+                hourlist = linelist[-1].split(':')
                 try:
                     linelist[-1] = [int(hourlist[0]),int(hourlist[1])]
                 except ValueError:
-                        print("Bad config format at line '{}'!"\
+                        print("Bad config format at line '{}'!\n"\
                                 .format(line.strip()),file = log)
                         exit()
                 booklist.append(linelist)
     return booklist
+log = initlog()
 
 
